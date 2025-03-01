7559ee5fa71fcbc3af29022853c22e5b89d6e11c
Vadhi Prudvi <81900665+prudvivadhi@users.noreply.github.com>
2025-03-01 08:57:41 +0530

    Update skincancer_web.py

--- skincancer_web.py
+++ skincancer_web.py
@@ -1,10 +1,6 @@
 # Step 1: Importing Essential Libraries
 import pandas as pd
 import numpy as np
-import tensorflow as tf
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Dropout
-from tensorflow.keras.utils import to_categorical
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder, StandardScaler
 from sklearn.metrics import classification_report
